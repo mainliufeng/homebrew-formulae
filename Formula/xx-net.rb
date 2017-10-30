e30841ce73f3237856f50e313f940a003b093bf8
刘峰 <mainliufeng@gmail.com>
2017-10-30 11:35:55 +0800

    update xx-net version to 3.7.7

--- Formula/xx-net.rb
+++ Formula/xx-net.rb
@@ -1,10 +1,9 @@
 class XxNet < Formula
   desc "a web proxy tool"
   homepage "https://github.com/XX-net/XX-Net"
-  url "https://codeload.github.com/XX-net/XX-Net/zip/3.3.0"
-  version "3.3.0"
-  sha256 "078dbaa301e877ef36e835d837023341949dc6b28b8ac0a61c15cfbacd43bcfe"
-
+  url "https://codeload.github.com/XX-net/XX-Net/zip/3.7.7"
+  version "3.7.7"
+  sha256 "d6793c5f68db01e2f59b747a663f63341f2b01af4223ee1050b5e2238aaf3cd0"
   depends_on :python
 
   def install
